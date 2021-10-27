d9fb84ec23e1479803eb9b490177e251770899f8
AM98-20 <alexmrtnz902@gmail.com>
2021-10-26 19:47:15 -0600

    API final v2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const passport = require('passport');
 const app = express();
 app.set('port', process.env.port || 3001);
 app.use(morgan('dev'));
 app.set(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.set('json space', 2);
+app.use(passport.initialize());
 app.listen(app.get('port'), () => {
     console.log('Servidor iniciado en el puerto ' + app.get('port'));
 });
